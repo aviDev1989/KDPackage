7facd28bb650950775fdb335ed3e3b4db014a2b9
aviDev1989 <148426563+aviDev1989@users.noreply.github.com>
2023-10-20 11:11:23 +0530

    Add Packages

--- Package.swift
+++ Package.swift
@@ -15,14 +15,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/gkaimakas/SwiftValidators.git", branch: "master")
+        .package(url: "https://github.com/gkaimakas/SwiftValidators.git", branch: "master"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", branch: "4.0.6")
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KDFontFramework",
-            dependencies: [],
+            dependencies: ["Starscream"],
             resources: [.process("Submit.storyboard")]),
         .testTarget(
             name: "KDFontFrameworkTests",
--- Sources/KDFontFramework/ViewController/SubmitVC/SubmitVC.swift
+++ Sources/KDFontFramework/ViewController/SubmitVC/SubmitVC.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import Starscream
 
 public class SubmitVC: UIViewController {
     public static let storyboardVC = UIStoryboard(name: "SubmitVC", bundle: Bundle.module).instantiateInitialViewController()
     @IBOutlet weak var lblMessage: UILabel!
     @IBOutlet weak var txtfldInput: UITextField!
+
+    
+    var socket: WebSocket?
+    var delegate : WebSocketDelegate?
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,4 +38,88 @@ public class SubmitVC: UIViewController {
     }
     */
 
+}
+extension SubmitVC{
+
+    func socketConnect(){
+        var userID : Int = 0
+        userID = 2323
+        let socketUrl = "ws://182.72.246.251:1111/"
+        let request = URLRequest(url: URL(string: socketUrl+"ws/chat/\(userID)\("/")")!)
+        print(request)
+        socket = WebSocket(request: request)
+        socket?.delegate = self
+        socket?.connect()
+
+    }
+    func setupPay(amt: Int, type: Int) -> Void {
+        let amount = amt
+        let room_id = 1232
+        let sender = 2344343
+        let transaction_type = type
+        var param = [String:AnyObject]()
+        param.updateValue(amount as AnyObject, forKey: "amount")
+        param.updateValue(room_id as AnyObject, forKey: "room_id")
+        param.updateValue(sender as AnyObject, forKey: "sender")
+        param.updateValue(transaction_type as AnyObject, forKey: "transaction_type")
+        let jsonData = try? JSONSerialization.data(withJSONObject: param, options: [])
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        socket?.write(string: jsonString!)
+    }
+    
+    func setupStatus(idss: Int, transaction_status: Int) -> Void {
+        var param = [String:AnyObject]()
+        param.updateValue(idss as AnyObject, forKey: "id")
+        param.updateValue(transaction_status as AnyObject, forKey: "transaction_status")
+        let jsonData = try? JSONSerialization.data(withJSONObject: param, options: [])
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        socket?.write(string: jsonString!)
+    }
+
+    
+    func didReceive(event: WebSocketEvent, client: WebSocket) {
+        switch event {
+        case .connected(let headers):
+          print("connected \(headers)")
+          socket?.connect()
+        case .disconnected(let reason, let closeCode):
+          print("disconnected \(reason) \(closeCode)")
+          socket?.disconnect()
+        case .text(let text):
+//            listenSocketForPay(txt: text)
+          print("received text: \(text)")
+        case .binary(let data):
+          print("Received data: \(data)")
+        case .pong(let pongData):
+          print("received pong: \(String(describing: pongData))")
+        case .ping(let pingData):
+          print("received ping: \(String(describing: pingData))")
+        case .error(let error):
+          print("error \(String(describing: error))")
+        case .viabilityChanged:
+          print("viabilityChanged")
+        case .reconnectSuggested:
+          print("reconnectSuggested")
+        case .cancelled:
+          print("cancelled")
+        case .peerClosed:
+            print("peerClosed")
+        }
+      }
+}
+// MARK: - WebSocketDelegate
+
+extension SubmitVC : WebSocketDelegate {
+    public func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient) {
+        
+    }
+    
+  public func websocketDidConnect(_ socket: Starscream.WebSocket) {
+  }
+  public func websocketDidDisconnect(_ socket: Starscream.WebSocket, error: NSError?) {
+  }
+  public func websocketDidReceiveMessage(_ socket: Starscream.WebSocket, text: String) {
+  }
+  public func websocketDidReceiveData(_ socket: Starscream.WebSocket, data: Data) {
+  }
 }
